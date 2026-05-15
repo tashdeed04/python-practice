b62d8f98afc73d6f49c6a310455982ab4e76a869
Tashdeed Noor Khan <tashdeed04@gmail.com>
2026-05-15 20:53:11 +0600

    Delete algorithms directory

--- algorithms/README.md
+++
@@ -1,3 +0,0 @@
-# Algorithms
-
-Python algorithm and problem solving practice including sorting, searching, arrays, and basic competitive programming problems.
--- algorithms/SumOfNumbers.py.py
+++
@@ -1,6 +0,0 @@
-import sys
-T = int(sys.stdin.readline())
-for i in range(T):
-    N = int(sys.stdin.readline())
-    res = N*(N+1)//2
-    sys.stdout.write(f"{res}\n")
